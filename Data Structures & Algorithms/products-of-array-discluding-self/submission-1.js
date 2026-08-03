1ce19212e65d1f51cc3c205e96b7a079db3821a9
Kalkidan shewit <kalkidan.ugr-2463-17@aau.edu.et>
2026-08-03 22:55:38 +0300

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+         let output = new Array(nums.length).fill(1);
+
+    let prefix = 1;
+
+    // Store product of everything on the left
+    for (let i = 0; i < nums.length; i++) {
+        output[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    let postfix = 1;
+
+    // Multiply by product of everything on the right
+    for (let i = nums.length - 1; i >= 0; i--) {
+        output[i] *= postfix;
+        postfix *= nums[i];
+    }
+
+    return output;
+    }
+}
